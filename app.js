f35c17275b7bf312b0bffb1db320f72c9d67a446
riad360 <shahibshadman@gmail.com>
2022-01-17 11:16:27 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,615 @@
+const phone = [{
+        sl: 1001,
+        name: 'Md Shahib Shadman',
+        age: 25,
+        location: 'Comilla',
+        phoneBook: 57185861002
+    },
+    {
+        sl: 1002,
+        name: 'Harunur Rashid',
+        age: 35,
+        location: 'Comilla',
+        phoneBook: 5718586458
+    },
+    {
+        sl: 1003,
+        name: 'Mamunr Rashid',
+        age: 30,
+        location: 'Dhaka',
+        phoneBook: 5718545877
+    },
+    {
+        sl: 1004,
+        name: 'Abu Sufian',
+        age: 34,
+        location: 'Comilla',
+        phoneBook: 571855896
+    },
+    {
+        sl: 1005,
+        name: 'Azam Khan',
+        age: 18,
+        location: 'Dhaka',
+        phoneBook: 571851257
+    },
+    {
+        sl: 1006,
+        name: 'Nur Amin',
+        age: 33,
+        location: 'Dhaka',
+        fees: 1500,
+    },
+    {
+        sl: 1007,
+        name: 'Abu Rahian',
+        age: 24,
+        location: 'Dhaka',
+        phoneBook: 5718584587
+    },
+    {
+        sl: 1008,
+        name: 'Shimul Raham',
+        age: 23,
+        location: 'Dhaka',
+        phoneBook: 571815789
+    },
+    {
+        sl: 1009,
+        name: 'Harunur Rashid',
+        age: 45,
+        location: 'Comilla',
+        phoneBook: 571805789
+    },
+    {
+        sl: 1010,
+        name: 'Somrat Khan',
+        age: 19,
+        location: 'Comilla',
+        phoneBook: 5718535784
+    },
+];
+
+
+const resultSheet = [{
+        name: 'Md Shahib Shadman',
+        id: 1540,
+        roll: 14,
+        age: 14,
+        class: '5th',
+        section: 'B',
+        blood: 'O+',
+
+        bangla: 80,
+        english: 90,
+        math: 95,
+        acc: 85,
+        man: 75,
+        rel: 88,
+        eco: 55
+    },
+    {
+        name: 'Hasnat Al Khan',
+        id: 1647,
+        roll: 15,
+        age: 10,
+        class: '8th',
+        section: 'D',
+        blood: 'O-',
+
+        bangla: 60,
+        english: 55,
+        math: 87,
+        acc: 85,
+        man: 95,
+        rel: 80,
+        eco: 30
+    },
+    {
+        name: 'Abu Bakkar',
+        id: 1547,
+        roll: 10,
+        age: 13,
+        class: '9th',
+        section: 'C',
+        blood: 'O-',
+
+        bangla: 60,
+        english: 45,
+        math: 57,
+        acc: 85,
+        man: 35,
+        rel: 80,
+        eco: 75
+    },
+
+];
+
+const studentData = [
+
+    {
+
+        id: 01,
+        name: ' Md Shahib Shadman',
+        age: 10,
+        location: 'Banani'
+    },
+    {
+
+        id: 02,
+        name: ' Abu Bakkar',
+        age: 10,
+        location: 'Banani'
+    },
+    {
+
+        id: 03,
+        name: ' Abu sufian',
+        age: 10,
+        location: 'comilla'
+    },
+    {
+
+        id: 04,
+        name: ' Abu rahain',
+        age: 10,
+        location: 'comilla'
+    },
+    {
+
+        id: 05,
+        name: ' Abul Kalma',
+        age: 10,
+        location: 'dinajpur'
+    },
+    {
+
+        id: 06,
+        name: ' Abul Kasem',
+        age: 10,
+        location: 'dinajpur'
+    },
+    {
+
+        id: 07,
+        name: ' Abdul Kudus',
+        age: 10,
+        location: 'Banani'
+    },
+    {
+
+        id: 08,
+        name: ' samad',
+        age: 10,
+        location: 'dinajpur'
+    },
+    {
+
+        id: 09,
+        name: ' Siaf Ali',
+        age: 10,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 10,
+        name: ' Sayeed',
+        age: 10,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 11,
+        name: ' mizan',
+        age: 10,
+        location: 'comilla'
+    },
+    {
+
+        id: 12,
+        name: ' shimul',
+        age: 10,
+        location: 'Banani'
+    },
+    {
+
+        id: 13,
+        name: ' hasnat',
+        age: 10,
+        location: 'Banani'
+    },
+    {
+
+        id: 14,
+        name: ' Ujjol',
+        age: 10,
+        location: 'comilla'
+    },
+    {
+
+        id: 15,
+        name: ' Rahim',
+        age: 10,
+        location: 'comilla'
+    },
+    {
+
+        id: 16,
+        name: ' Shoung',
+        age: 10,
+        location: 'comilla'
+    },
+    {
+
+        id: 17,
+        name: ' Jakir',
+        age: 10,
+        location: 'comilla'
+    },
+    {
+
+        id: 18,
+        name: ' Shakil',
+        age: 10,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 19,
+        name: ' Ram Jebon',
+        age: 10,
+        location: 'Banani'
+    },
+    {
+
+        id: 20,
+        name: ' Krisna',
+        age: 10,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 21,
+        name: ' Rashed',
+        age: 10,
+        location: 'dinajpur'
+    },
+    {
+
+        id: 22,
+        name: ' Rakib',
+        age: 10,
+        location: 'dinajpur'
+    },
+    {
+
+        id: 23,
+        name: ' Rabbi',
+        age: 10,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 24,
+        name: ' sufian',
+        age: 10,
+        location: 'Banani'
+    },
+    {
+
+        id: 25,
+        name: ' sufian',
+        age: 10,
+        location: 'comilla'
+    },
+    {
+
+        id: 26,
+        name: ' uppo',
+        age: 10,
+        location: 'comilla'
+    },
+    {
+
+        id: 27,
+        name: ' mehedi',
+        age: 10,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 28,
+        name: ' Polash',
+        age: 10,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 29,
+        name: ' Somun',
+        age: 10,
+        location: 'Banani'
+    },
+    {
+
+        id: 30,
+        name: ' Mamun',
+        age: 10,
+        location: 'dinajpur'
+    },
+    {
+
+        id: 31,
+        name: ' Nu Amin',
+        age: 10,
+        location: 'dinajpur'
+    },
+    {
+
+        id: 32,
+        name: ' Somrat',
+        age: 10,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 33,
+        name: ' Sabbir',
+        age: 15,
+        location: 'comilla'
+    },
+    {
+
+        id: 34,
+        name: ' Jillur ',
+        age: 15,
+        location: 'Banani'
+    },
+    {
+
+        id: 35,
+        name: ' Asraf',
+        age: 19,
+        location: 'comilla'
+    },
+    {
+
+        id: 36,
+        name: ' PAbel',
+        age: 18,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 37,
+        name: ' Pabel',
+        age: 14,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 38,
+        name: ' Shoung',
+        age: 5,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 39,
+        name: ' Robel',
+        age: 5,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 40,
+        name: ' Rahim',
+        age: 6,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 41,
+        name: ' Kuddos',
+        age: 9,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 42,
+        name: ' Delower',
+        age: 11,
+        location: 'Banani'
+    },
+    {
+
+        id: 43,
+        name: ' Osman Goni',
+        age: 15,
+        location: 'comilla'
+    },
+    {
+
+        id: 44,
+        name: ' Omar faruk',
+        age: 15,
+        location: 'comilla'
+    },
+    {
+
+        id: 45,
+        name: ' Tanvir',
+        age: 8,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 46,
+        name: ' Tafsir',
+        age: 8,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 47,
+        name: ' Nazmul',
+        age: 12,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 48,
+        name: ' Foysal ',
+        age: 12,
+        location: 'Banani'
+    },
+    {
+
+        id: 49,
+        name: ' Rokun Uddin',
+        age: 7,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 50,
+        name: ' Abdul Momin',
+        age: 7,
+        location: 'Mirpur'
+    },
+    {
+
+        id: 51,
+        name: ' Abu Sayeed Rifat',
+        age: 6,
+        location: 'Banani'
+    },
+
+];
+
+const devs = [
+    {
+        ID         : 01,
+        name       : 'Md Shahib Shadman',
+        age        : 25,
+        bloodGroup : 'O+',
+        skill      : 'Wordpress',
+        location   : 'comilla',
+        gender     : 'male',
+        Salary     : 1400
+
+    },
+    {
+        ID         : 02,
+        name       : 'Abu Bakkar ',
+        age        : 29 ,
+        bloodGroup : 'A+',
+        skill      : ' Laravel',
+        location   : 'Dhaka',
+        gender     : 'male',
+        Salary     : 1800
+
+    },
+    {
+        ID         : 03,
+        name       : 'Mamun Al Rashid',
+        age        : 32 ,
+        bloodGroup : 'AB+',
+        skill      : ' Django',
+        location   : 'Dinajpur',
+        gender     : 'male',
+        Salary     : 3600
+
+    },
+    {
+        ID         : 04,
+        name       : 'Nur Amin ',
+        age        : 35 ,
+        bloodGroup : 'AB-',
+        skill      : ' Golan',
+        location   : 'comilla',
+        gender     : 'male',
+        Salary     : 900
+
+    },
+    {
+        ID         : 05,
+        name       : 'Abu Rahain',
+        age        : 28 ,
+        bloodGroup : 'B-',
+        skill      : ' MERN stack',
+        location   : 'Dhaka',
+        gender     : 'male',
+        Salary     : 5100
+
+    },
+    {
+        ID         : 06,
+        name       : 'Abu Rahain',
+        age        : 28 ,
+        bloodGroup : 'B-',
+        skill      : ' MERN stack',
+        location   : 'Dinajpur',
+        gender     : 'male',
+        Salary     : 3400
+
+    },
+    {
+        ID         : 07,
+        name       : 'Abu Sufain ',
+        age        : 38 ,
+        bloodGroup : 'A-',
+        skill      : ' JavaScprit',
+        location   : 'comilla',
+        gender     : 'male',
+        Salary     : 2900
+
+    },
+    
+    {
+        ID         : 08,
+        name       : 'Abul Kalam ',
+        age        : 45 ,
+        bloodGroup : 'A+',
+        skill      : ' JavaScprit',
+        location   : 'comilla',
+        gender     : 'male',
+        Salary     : 3200
+
+    },
+    
+    {
+        ID         : 09,
+        name       : 'Morioum  ',
+        age        : 65 ,
+        bloodGroup : 'A+',
+        skill      : ' JavaScprit',
+        location   : 'comilla',
+        gender     : 'Female',
+        Salary     : 1200
+    },
+    {
+        ID         : 10,
+        name       : 'Asma Akter  ',
+        age        : 20 ,
+        bloodGroup : 'O+',
+        skill      : ' Wordpress',
+        location   : 'dinajpur',
+        gender     : 'Female',
+        Salary     : 4000
+    },
+    
+];
+
+let phoneDataJson = JSON.stringify(phone);
+
+localStorage.setItem('PhoneData', phoneDataJson)
+
+let studentResultSheet = JSON.stringify(resultSheet);
+localStorage.setItem('StudentResultSheet', studentResultSheet)
+
+let studentDataJson = JSON.stringify(studentData);
+localStorage.setItem('StudentData', studentDataJson)
+
+let developerData =JSON.stringify(devs);
+localStorage.setItem('DeveloperData', developerData)
